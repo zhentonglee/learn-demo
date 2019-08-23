f3c645259a1a534f1306a6ee20cee4c41e4575ff
zhentonglee <2909269864@qq.com>
2019-08-23 23:33:40 +0800

    update

--- 06-node-blog/blog-native/app.js
+++ 06-node-blog/blog-native/app.js
@@ -1,15 +1,37 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 const serverHandle = (req, res) => {
 	//设置返回格式 JSON
 	res.setHeader('Content-type', 'application/json')
-	const resData = {
-		name: 'tomli',
-		site: 'izhong',
-		env: process.env.NODE_ENV
+
+	//获取path
+	const url = req.url
+    req.path = url.split('?')[0]
+	
+	//处理blog路由
+	const blogData = handleBlogRouter(req, res)
+	if(blogData) {
+		res.end(
+			JSON.stringify(blogData)
+		)
+		return
 	}
 
-	res.end(
-		JSON.stringify(resData)
-	)
+	//处理user路由
+	const userData = handleUserRouter(req, res)
+	if(userData) {
+		res.end(
+			JSON.stringify(userData)
+		)
+		return
+	}
+
+	//未命中路由，返回404
+	res.writeHead(404, {'Content-type':'text/plain'})
+	res.write('404 not found\n')
+	res.end()
+
 }
 
 module.exports = serverHandle
