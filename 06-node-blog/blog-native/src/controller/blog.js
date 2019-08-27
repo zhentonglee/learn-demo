a3967e9671dd5ec1b5bcab85080900800674083c
zhentonglee <2909269864@qq.com>
2019-08-28 01:03:53 +0800

    使用数据库

--- 06-node-blog/blog-native/src/controller/blog.js
+++ 06-node-blog/blog-native/src/controller/blog.js
@@ -1,37 +1,34 @@
+const { exec } = require('../db/mysql')
+
 const getList = (author, keyword) => {
-    //先返回假数据
-    return [
-        {
-            id: 1,
-            title: '标题A',
-            content: '内容A',
-            createTime: '1566622829393',
-            author: 'tom'
-        },
-        {
-            id: 2,
-            title: '标题B',
-            content: '内容B',
-            createTime: '1566622897873',
-            author: 'jerry'
-        }
-    ]
+    let sql = `select * from blogs where 1=1 `
+    if(author) {
+        sql += `and author='${author}' `
+    }
+    if(keyword) {
+        sql += `and title like '%${keyword}%' `
+    }
+    sql += `order by createtime desc;`
+
+    return exec(sql)
 }
 
 const getDetail = (id) => {
-    return {
-        id: 1,
-        title: '标题A',
-        content: '内容A',
-        createTime: '1566622829393',
-        author: 'tom'
-    }
+    const sql = `select * from blogs where id='${id}'`
+    return exec(sql).then(rows => {
+        return rows[0]
+    })
 }
 
 const newBlog = (blogData = {}) => {
-    return {
-        id: 3
-    }
+    const title = blogData.title, content = blogData.content, author = blogData.author, createtime = Date.now()
+    const sql = `insert into blogs (title, content, author, createtime) values ('${title}', '${content}', '${author}', ${createtime});`
+    return exec(sql).then(insertData => {
+        console.log(insertData)
+        return {
+            id: insertData.insertId
+        }
+    })
 }
 
 const updateBlog = (id, blogData = {}) => {
