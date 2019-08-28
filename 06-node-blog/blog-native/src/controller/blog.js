b3dd95b31a3c2fc67327f3b30b6529ef0a2ff5ba
zhentonglee <2909269864@qq.com>
2019-08-28 12:54:08 +0800

    update

--- 06-node-blog/blog-native/src/controller/blog.js
+++ 06-node-blog/blog-native/src/controller/blog.js
@@ -32,11 +32,24 @@ const newBlog = (blogData = {}) => {
 }
 
 const updateBlog = (id, blogData = {}) => {
-    return true
+    const title = blogData.title, content = blogData.content
+    const sql = `update blogs set title='${title}', content='${content}' where id='${id}';`
+    return exec(sql).then(updateData => {
+        if(updateData.affectedRows > 0) {
+            return true
+        }
+        return false
+    })
 }
 
-const delBlog = (id) => {
-    return true
+const delBlog = (id, author) => {
+    const sql = `delete from blogs where id='${id}' and author='${author}';`
+    return exec(sql).then(delData => {
+        if(delData.affectedRows > 0) {
+            return true
+        }
+        return false
+    })
 }
 
 module.exports = {
