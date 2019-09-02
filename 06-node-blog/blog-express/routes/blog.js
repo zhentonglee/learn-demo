4526b2b30b65bd040448f041ad9f563860cc5fd9
zhentonglee <2909269864@qq.com>
2019-09-02 09:38:51 +0800

    update

--- 06-node-blog/blog-express/routes/blog.js
+++ 06-node-blog/blog-express/routes/blog.js
@@ -2,10 +2,21 @@ var express = require('express');
 var router = express.Router();
 const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 
-router.get('/list', function(req, res, next) {
+router.get('/list', (req, res, next) => {
     let author = req.query.author || ''
     const keyword = req.query.keyword || ''
+
+    if(req.query.isAdmin) {
+        if(req.session.username == null) {
+            res.json(
+                new ErrorModel(listData)
+            )
+            return
+        }
+        author = req.session.username
+    }
 
     const result = getList(author, keyword)
     return result.then(listData => {
@@ -15,10 +26,22 @@ router.get('/list', function(req, res, next) {
     })
 })
 
-router.get('/detail', function(req, res, next) {
-    res.json({
-        errno: 0,
-        data: ok
+router.get('/detail', (req, res, next) => {
+    const result = getDetail(req.query.id)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
+    })
+})
+
+router.post('/new', loginCheck, (req, res, next) => {
+    req.body.author = req.session.username
+    const result = newBlog(req.body)
+    return result.then(data => {
+        res.json(
+            new SuccessModel(data)
+        )
     })
 })
 
