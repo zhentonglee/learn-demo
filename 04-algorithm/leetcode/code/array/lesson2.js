34c50dd17833ed7ad06cd7d6384f1e86bc3703ea
zhentonglee <2909269864@qq.com>
2019-09-29 02:03:32 +0800

    优化

--- 04-algorithm/leetcode/code/array/lesson2.js
+++ 04-algorithm/leetcode/code/array/lesson2.js
@@ -1,31 +1,34 @@
 export default (arr) => {
+    if (arr.length <= 1) return false
     // 对这副牌进行排序，升序、降序都可以
     arr.sort((a, b) => a - b)
     let min = Number.MAX_SAFE_INTEGER
     let dst = []
     let result = true
     for (let i = 0, len = arr.length, tmp = []; i < len; i++) {
-      tmp.push(arr[i])
-      for (let j = i + 1; j < len - 1; j++) {
-        if (arr[i] === arr[j]) {
-          tmp.push(arr[j])
-        } else {
-          if (min > tmp.length) {
-            min = tmp.length
-          }
-          dst.push([].concat(tmp))
-          tmp.length = 0
-          i = j
-          break
+        tmp.push(arr[i])
+        for (let j = i + 1; j < len; j++) {
+            if (arr[i] === arr[j]) {
+                tmp.push(arr[j])
+            } else {
+                i = j - 1
+                break
+            }
+            if (j === len - 1) { 
+                i = j 
+            }
         }
-      }
+        if (min > tmp.length) {
+            min = tmp.length
+        }
+        dst.push([].concat(tmp))
+        tmp.length = 0
     }
     dst.every(item => {
-      if (item.length % min !== 0) {
-        result = false
-        return false
-      }
+        if (item.length % min !== 0 || item.length <= 1) {
+            result = false
+            return false
+        }
     })
     return result
-  }
-  
+}
