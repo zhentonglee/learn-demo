7a83057241f94b3e0883efc74248ff32d6658e7f
zhentonglee <2909269864@qq.com>
2019-10-05 13:10:14 +0800

    '最大间距'

--- 04-algorithm/leetcode/test/sort/lesson1.test.js
+++ 04-algorithm/leetcode/test/sort/lesson1.test.js
@@ -1,4 +1,4 @@
-import sort from '../../code/sort/lesson1-1'
+import sort from '../../code/sort/lesson1'
 
 test('冒泡排序', () => {
     expect(sort([1, 9, 5, 3, 4])).toEqual([1, 3, 4, 5, 9])
--- 04-algorithm/leetcode/test/sort/lesson2.test.js
+++ 04-algorithm/leetcode/test/sort/lesson2.test.js
@@ -1,4 +1,4 @@
-import sort from '../../code/sort/lesson1-2'
+import sort from '../../code/sort/lesson2'
 
 test('选择排序', () => {
     expect(sort([1, 9, 5, 3, 4])).toEqual([1, 3, 4, 5, 9])
