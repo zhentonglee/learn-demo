959b785ef72981a7474c0f996b2ff440edad7a9b
zhentonglee <2909269864@qq.com>
2019-11-25 23:47:32 +0800

    模块打包

---
+++ 05-webpack/lesson/src/hellowebpack.js
@@ -0,0 +1,3 @@
+export default function hellowebpack() {
+    return 'hello webpack';
+}
---
+++ 05-webpack/lesson/src/index.js
@@ -0,0 +1,4 @@
+import { helloworld, hellogirls } from './helloworld';
+import hellowebpack from './hellowebpack';
+
+document.write(`${helloworld()} + ${hellogirls()} + ${hellowebpack()}`);
