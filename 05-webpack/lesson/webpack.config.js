fd9ea7c8c873af27ad0e7d7ab12e16edd695a050
zhentonglee <2909269864@qq.com>
2019-11-27 06:34:42 +0800

    '解析ES6、JSX'

--- 05-webpack/lesson/webpack.config.js
+++ 05-webpack/lesson/webpack.config.js
@@ -3,10 +3,21 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        search: './src/search.js'
+    },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
-    mode: 'production'
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    }
 }
